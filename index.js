aaf22e3f1990a5427c5000710e5bbb6f878baaff
Justin Locsei <justin.locsei@gmail.com>
2015-12-16 19:30:31 -0500

    Decouple server creation and binding

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var http = require('http');
+
 var cliArgs = require('./config/cli-args');
 var paths = require('./config/paths');
 var routes = require('./config/routes');
@@ -9,9 +11,9 @@ var resources = require('./config/resources');
 var options = cliArgs.parse();
 var servers = resources.servers(options.environment);
 
-server.serve({
-  host: servers.api.host,
-  port: servers.api.port,
+var app = server.create({
   routes: routes,
   templates: paths.ui.templates
 });
+
+http.createServer(app).listen(servers.api.port, servers.api.host);
--- server/server.js
+++ server/server.js
@@ -7,19 +7,16 @@ var nunjucks = require('nunjucks');
 var endpoints = require('./endpoints');
 
 /**
- * Start the API server
+ * Create an instance of an API server
  *
  * @param {Object} options Configuration for the server
- * @param {string} options.host The host on which to listen
- * @param {number} options.port The port on which to listen
  * @param {Object} options.routes A mapping of route names to URLs
  * @param {string} options.templates The path to the templates directory
+ * @returns {Server} An API server that can be bound to an address
  */
-function serve(options) {
+function create(options) {
   var settings = options || {};
 
-  if (!settings.host) { throw new Error('You must specify a host for the server'); }
-  if (!settings.port) { throw new Error('You must specify a port for the server'); }
   if (!settings.routes) { throw new Error('You must specify a route resolver for the server'); }
   if (!settings.templates) { throw new Error('You must specify a path to the templates directory'); }
 
@@ -36,9 +33,9 @@ function serve(options) {
   var routes = settings.routes;
   app.get(routes.urls.index, endpoints.index);
 
-  return app.listen(settings.port, settings.host);
+  return app;
 }
 
 module.exports = {
-  serve: serve
+  create: create
 };
