73410bbfb8824d76ac5f05f85b9256fc5f508aa3
Justin Locsei <justin.locsei@gmail.com>
2015-12-16 19:12:21 -0500

    Expose URLs and resolver functions for the routes

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,16 @@
 'use strict';
 
-module.exports = {
+// A mapping of route names to URLs
+var urls = {
   index: '/'
+};
+
+// A map of route names to functions that return a URL
+var resolve = {
+
+};
+
+module.exports = {
+  resolve: resolve,
+  urls: urls
 };
