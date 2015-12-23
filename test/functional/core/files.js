841cfea526a6f743c0d4282d04a2aa62938df79c
Justin Locsei <justin.locsei@gmail.com>
2015-12-23 16:09:48 -0500

    Add tests for the core code

---
+++ test/functional/core/files.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var files = require('chiton/core/files');
+
+describe('files', function() {
+
+  describe('.deep', function() {
+    it('returns a glob that recursively match files in a directory', function() {
+      var glob = files.deep('test/path', 'js');
+      assert.equal(glob, 'test/path/**/*.js');
+    });
+
+    it('matches all files when no extension is given', function() {
+      var glob = files.deep('test/path');
+      assert.equal(glob, 'test/path/**/*.*');
+    });
+  });
+
+  describe('.shallow', function() {
+    it('returns a global that matches files in a directory', function() {
+      var glob = files.shallow('test/path', 'js');
+      assert.equal(glob, 'test/path/*.js');
+    });
+
+    it('matches all files when no extension is given', function() {
+      var glob = files.shallow('test/path');
+      assert.equal(glob, 'test/path/*.*');
+    });
+  });
+
+});
---
+++ test/functional/core/paths.js
@@ -0,0 +1,96 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+
+var paths = require('chiton/core/paths');
+
+describe('paths', function() {
+
+  describe('the root directory', function() {
+    it('exists', function() {
+      assert(isDirectory(paths.root));
+    });
+  });
+
+  describe('the build directory', function() {
+    it('is below the root directory', function() {
+      assert(isBelow(paths.build.root, paths.root));
+    });
+
+    it('is not the source directory', function() {
+      assert.notEqual(paths.build.root, paths.src);
+    });
+
+    it('contains a directory for server builds', function() {
+      assert(isBelow(paths.build.server, paths.build.root));
+    });
+
+    it('contains a directory for UI builds', function() {
+      assert(isBelow(paths.build.ui, paths.build.root));
+    });
+  });
+
+  describe('the source directory', function() {
+    it('exists', function() {
+      assert(isDirectory(paths.src));
+    });
+
+    it('is below the root directory', function() {
+      assert(isBelow(paths.src, paths.root));
+    });
+
+    it('contains a directory for server code', function() {
+      assert(isBelow(paths.server, paths.src));
+      assert(isDirectory(paths.server));
+    });
+
+    it('contains a directory for UI code', function() {
+      assert(isBelow(paths.ui.root, paths.src));
+      assert(isDirectory(paths.ui.root));
+    });
+
+    describe('the UI directory', function() {
+      it('contains a directory for JS files', function() {
+        assert(isBelow(paths.ui.js, paths.ui.root));
+        assert(isDirectory(paths.ui.js));
+      });
+
+      it('contains a directory for Sass files', function() {
+        assert(isBelow(paths.ui.scss, paths.ui.root));
+        assert(isDirectory(paths.ui.scss));
+      });
+
+      it('contains a directory for templates', function() {
+        assert(isBelow(paths.ui.templates, paths.ui.root));
+        assert(isDirectory(paths.ui.templates));
+      });
+    });
+  });
+
+  describe('the test directory', function() {
+    it('exists', function() {
+      assert(isDirectory(paths.test.root));
+    });
+
+    it('is below the root directory', function() {
+      assert(isBelow(paths.test.root, paths.root));
+    });
+
+    it('contains a directory for functional tests', function() {
+      assert(isBelow(paths.test.functional, paths.test.root));
+      assert(isDirectory(paths.test.functional));
+    });
+  });
+
+});
+
+function isBelow(directory, container) {
+  var relative = path.relative(directory, container);
+  return /^\.{2}/.test(relative);
+}
+
+function isDirectory(directory) {
+  var stats = fs.statSync(directory);
+  return stats.isDirectory();
+}
