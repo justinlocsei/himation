6ac64cc9904f29cee084a9231343b8a75c93c48e
Justin Locsei <justin.locsei@gmail.com>
2015-12-22 19:26:51 -0500

    Allow the main server to be started via a function

--- index.js
+++ index.js
@@ -1,33 +1,5 @@
 'use strict';
 
-var http = require('http');
-var https = require('https');
+var server = require('./server');
 
-var cliArgs = require('./config/cli-args');
-var environments = require('./config/environments');
-var paths = require('./config/paths');
-var routes = require('./config/routes');
-var server = require('./server/server');
-var urls = require('./server/urls');
-
-var options = cliArgs.parse();
-
-var settings = environments.loadSettings(options.environment);
-var servers = settings.servers;
-
-var assetUrl = urls.expandHost(servers.assets.host, {
-  port: servers.assets.port,
-  protocol: servers.assets.protocol
-});
-
-var app = server.create({
-  assetUrl: assetUrl,
-  routes: routes,
-  templates: paths.ui.templates
-});
-
-var serverFactory = servers.api.protocol === 'https' ? https : http;
-serverFactory.createServer(app).listen(servers.api.port, servers.api.host, function() {
-  var address = this.address();
-  console.log('Server available at %s:%s', address.address, address.port);
-});
+server.start();
---
+++ server.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var http = require('http');
+var https = require('https');
+
+var application = require('./server/application');
+var cliArgs = require('./config/cli-args');
+var environments = require('./config/environments');
+var paths = require('./config/paths');
+var routes = require('./config/routes');
+var urls = require('./server/urls');
+
+/**
+ * Start the server
+ *
+ * @returns {Server} The listening server
+ */
+function start() {
+  var options = cliArgs.parse();
+
+  var settings = environments.loadSettings(options.environment);
+  var servers = settings.servers;
+
+  var assetUrl = urls.expandHost(servers.assets.host, {
+    port: servers.assets.port,
+    protocol: servers.assets.protocol
+  });
+
+  var app = application.create({
+    assetUrl: assetUrl,
+    routes: routes,
+    templates: paths.ui.templates
+  });
+
+  var serverFactory = servers.api.protocol === 'https' ? https : http;
+  var server = serverFactory.createServer(app);
+
+  server.listen(servers.api.port, servers.api.host, function() {
+    var address = this.address();
+    console.log('Server available at %s:%s', address.address, address.port);
+  });
+
+  return server;
+}
+
+module.exports = {
+  start: start
+};
