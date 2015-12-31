0228c2a7debc84ba69e39dae258de4fdcb1c13fe
Justin Locsei <justin.locsei@gmail.com>
2015-12-31 14:45:02 -0500

    Restructure the route definitions to use a recursive data structure

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,11 +1,19 @@
 'use strict';
 
 /**
- * @typedef {object} ChitonRoutes
+ * Chiton route definitions
+ *
+ * @typedef {object} ChitonRoute
+ * @property {string} name The internal ID for the route
+ * @property {string} url The path to the route
+ * @property {ChitonRoute[]} urls Child URLs within the route's namespace
  */
-var routes = {
-  index: '/',
-  about: '/about'
-};
+var routes = [{
+  name: 'chiton',
+  url: '/',
+  urls: [
+    {url: 'about', name: 'about'}
+  ]
+}];
 
 module.exports = routes;
