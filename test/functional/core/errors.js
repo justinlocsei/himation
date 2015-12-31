5202cb766627476ed117e07da1c107978734b1c8
Justin Locsei <justin.locsei@gmail.com>
2015-12-31 10:29:51 -0500

    Allow custom error classes to be created

---
+++ test/functional/core/errors.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var errors = require('chiton/core/errors');
+
+describe('errors', function() {
+
+  describe('.subclass', function() {
+    it('creates new error subclasses', function() {
+      var MyError = errors.subclass();
+      var error = new MyError();
+
+      assert.instanceOf(error, Error);
+      assert.instanceOf(error, MyError);
+    });
+
+    it('creates error classes that accept a message', function() {
+      var MyError = errors.subclass();
+      var error = new MyError('error message');
+
+      assert.equal(error.message, 'error message');
+    });
+
+    it('can accept a custom parent subclass', function() {
+      var ParentError = errors.subclass();
+      var ChildError = errors.subclass(ParentError);
+      var error = new ChildError();
+
+      assert.instanceOf(error, Error);
+      assert.instanceOf(error, ParentError);
+      assert.instanceOf(error, ChildError);
+    });
+  });
+
+});
