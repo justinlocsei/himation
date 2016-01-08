5b16c67a4a0667fd9a5a460a37b0e604c6b1fe8c
Justin Locsei <justin.locsei@gmail.com>
2016-01-08 11:30:54 -0500

    Restructure the assertion extensions in the test environment

--- test/support/environment.js
+++ test/support/environment.js
@@ -3,14 +3,15 @@
 var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
 
-var fs = require('./assertions/fs');
+var assertionModules = [
+  require('./assertions/fs')
+];
 
 chai.use(chaiAsPromised);
 
 var assert = global.assert = chai.assert;
 
 // Expose all assertions via the global assertion object
-var assertionModules = [fs];
 assertionModules.forEach(function(assertions) {
   Object.keys(assertions).forEach(function(assertion) {
     assert[assertion] = assertions[assertion];
