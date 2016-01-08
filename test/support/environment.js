8c53d1423644560f25810e0a411b420e7e216f5f
Justin Locsei <justin.locsei@gmail.com>
2016-01-08 11:46:02 -0500

    Use absolute paths for test assertions

--- test/support/environment.js
+++ test/support/environment.js
@@ -4,16 +4,16 @@ var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
 
 var assertionModules = [
-  require('./assertions/fs')
-];
+  'fs'
+].map(name => require('chiton-test/support/assertions/' + name));
 
 chai.use(chaiAsPromised);
 
 var assert = global.assert = chai.assert;
 
 // Expose all assertions via the global assertion object
-assertionModules.forEach(function(assertions) {
-  Object.keys(assertions).forEach(function(assertion) {
-    assert[assertion] = assertions[assertion];
+assertionModules.forEach(function(assertionFunctions) {
+  Object.keys(assertionFunctions).forEach(function(assertionFunction) {
+    assert[assertionFunction] = assertionFunctions[assertionFunction];
   });
 });
