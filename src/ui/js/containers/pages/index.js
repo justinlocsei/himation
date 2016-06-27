fcba4caedb2c21596b11bf913c0c556419f2582a
Justin Locsei <justin.locsei@gmail.com>
2016-06-26 20:02:10 -0400

    Submit the survey form to the recommendations page

--- src/ui/js/containers/pages/index.js
+++ src/ui/js/containers/pages/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 
 import Page from 'himation/ui/js/components/pages';
+import routes from 'himation/config/routes';
 import Site from 'himation/ui/js/containers/site';
 import Survey from 'himation/ui/js/components/survey';
+import { guidToRoute } from 'himation/core/routing';
 
 const HomePage = React.createClass({
 
   render: function() {
+    const submitSurvey = guidToRoute(routes, 'himation.recommendations');
+
     return (
       <Site>
         <Page title="Home">
-          <Survey />
+          <Survey formAction={submitSurvey.path} formMethod={submitSurvey.method} />
         </Page>
       </Site>
     );
