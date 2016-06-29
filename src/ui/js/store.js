caff4098e4d346ce685ac94e665eb2dd8a3bf2c9
Justin Locsei <justin.locsei@gmail.com>
2016-06-28 20:40:53 -0400

    Allow middleware to be used with the Redux store

--- src/ui/js/store.js
+++ src/ui/js/store.js
@@ -10,14 +10,15 @@ import appReducers from 'himation/ui/js/reducers';
  *
  * @param {React.Component} component An instance of a React component
  * @param {object} [initialState] The initial state of the store
+ * @param {object[]} [middleware] Middleware to add to the store
  * @returns {React.Component} The initial component, wrapped in a provider
  */
-export function bindAppToStore(component, initialState) {
+export function bindAppToStore(component, initialState, middleware) {
   const reducers = combineReducers({
     ...appReducers,
     form: formReducer
   });
 
-  const store = createStore(reducers, initialState);
+  const store = createStore(reducers, initialState, middleware);
   return React.createElement(Provider, {store: store}, component);
 }
