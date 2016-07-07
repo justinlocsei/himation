28497a9118ee5f131c1c765d76e8fcb39cfabd7e
Justin Locsei <justin.locsei@gmail.com>
2016-07-07 19:45:28 -0400

    Use consistent casing

--- src/ui/js/pages/about.js
+++ src/ui/js/pages/about.js
@@ -1,4 +1,4 @@
 import { renderPageComponent } from 'himation/ui/js/rendering';
-import Aboutpage from 'himation/ui/js/containers/pages/about';
+import AboutPage from 'himation/ui/js/containers/pages/about';
 
-renderPageComponent(Aboutpage);
+renderPageComponent(AboutPage);
