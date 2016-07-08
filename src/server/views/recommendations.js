d0eedaff7e479e1e507c8efba95554118d39c317
Justin Locsei <justin.locsei@gmail.com>
2016-07-07 20:28:44 -0400

    Use a dedicated function for rendering an invalid survey

--- src/server/views/recommendations.js
+++ src/server/views/recommendations.js
@@ -5,22 +5,32 @@ import { createApiClient } from 'himation/server/api';
 import { prerenderPageComponent } from 'himation/ui/js/rendering';
 import { validate } from 'himation/ui/js/components/survey';
 
+/**
+ * Render an invalid survey form
+ *
+ * @param {Response} res The server response
+ * @param {object} data The survey state shape
+ */
+function renderInvalidSurveyForm(res, data) {
+  res.render('pages/index', {
+    content: prerenderPageComponent(IndexPage, {
+      survey: {
+        ...data,
+        form: {
+          failedValidation: true,
+          isSubmitting: false
+        }
+      }
+    })
+  });
+}
+
 export function renderResponse(req, res, settings) {
   const surveyData = convertPostDataToStateShape(req.body);
   const surveyValidationErrors = validate(surveyData);
 
   if (Object.keys(surveyValidationErrors).length) {
-    return res.render('pages/index', {
-      content: prerenderPageComponent(IndexPage, {
-        survey: {
-          ...surveyData,
-          form: {
-            failedValidation: true,
-            isSubmitting: false
-          }
-        }
-      })
-    });
+    return renderInvalidSurveyForm(res, surveyData);
   }
 
   const apiClient = createApiClient(settings.chiton.endpoint, settings.chiton.token);
