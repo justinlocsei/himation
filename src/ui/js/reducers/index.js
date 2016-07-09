849323d64feddca13b12488713ea4bd3a312118a
Justin Locsei <justin.locsei@gmail.com>
2016-07-08 21:29:42 -0400

    Add a state shape for recommendations

--- src/ui/js/reducers/index.js
+++ src/ui/js/reducers/index.js
@@ -1,7 +1,9 @@
 import profile from './profile';
+import recommendations from './recommendations';
 import survey from './survey';
 
 export default {
   profile: profile,
+  recommendations: recommendations,
   survey: survey
 };
---
+++ src/ui/js/reducers/recommendations.js
@@ -0,0 +1,3 @@
+export default function recommendations(state = []) {
+  return state;
+}
