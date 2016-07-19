9e9604c9156bea38489a601bd5369c79bb6b2911
Justin Locsei <justin.locsei@gmail.com>
2016-07-18 21:13:45 -0400

    Rough in the markup and styles for a table of contents for basics

--- src/ui/js/components/recommendations/index.js
+++ src/ui/js/components/recommendations/index.js
@@ -12,15 +12,54 @@ const Recommendations = React.createClass({
   render: function() {
     const { basics } = this.props;
 
-    const basicsByName = sortBy(basics, basic => basic.basic.name);
+    const basicsByCategory = basics.reduce(function(previous, basic) {
+      const category = basic.basic.category;
+      if (!previous[category]) {
+        previous[category] = [];
+      }
+
+      previous[category].push({
+        name: basic.basic.name,
+        slug: basic.basic.slug
+      });
+
+      return previous;
+    }, {});
+
+    const sortedBasics = sortBy(basics, basic => basic.basic.name);
+    const sortedCategories = Object.keys(basicsByCategory).sort();
 
     return (
       <div className="l--recommendations">
-        <div className="l--recommendations__basics">
-          {basicsByName.map(function(basic, index) {
+        <div className="l--recommendations__categories">
+          {sortedCategories.map(function(category, categoryIndex) {
             return (
-              <section className="l--recommendations__basic" key={index}>
-                <Basic facets={basic.facets} garments={basic.garments} name={basic.basic.name} />
+              <section className="l--recommendations__category" key={categoryIndex}>
+                <h2 className="l--recommendations__category__name">{category}</h2>
+                <ul className="l--recommendations__category__basics">
+                  {basicsByCategory[category].map(function(basic, basicIndex) {
+                    return (
+                      <li className="l--recommendations__category__basic" key={basicIndex}>
+                        <a className="l--recommendations__category__basic__name" href={`#${basic.slug}`}>{basic.name}</a>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </section>
+            );
+          })}
+        </div>
+
+        <div className="l--recommendations__basics">
+          {sortedBasics.map(function(basic, index) {
+            return (
+              <section className="l--recommendations__basic" key={index} id={basic.basic.slug}>
+                <Basic
+                  category={basic.basic.category}
+                  facets={basic.facets}
+                  garments={basic.garments}
+                  name={basic.basic.name}
+                />
               </section>
             );
           })}
