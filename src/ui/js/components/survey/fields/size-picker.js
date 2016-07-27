eb23d5aa4f530e5639a1d362a9adf6aa2284348c
Justin Locsei <justin.locsei@gmail.com>
2016-07-26 20:55:44 -0400

    Rough in styles for the size picker

--- src/ui/js/components/survey/fields/size-picker.js
+++ src/ui/js/components/survey/fields/size-picker.js
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react';
 
-import { SIZES } from 'himation/core/data/survey';
+import { SIZE_GROUPS } from 'himation/core/data/survey';
 
 const SizePicker = React.createClass({
 
@@ -11,19 +11,6 @@ const SizePicker = React.createClass({
 
   render: function() {
     const { fields, id } = this.props;
-
-    const sizeTags = fields.map(function(field, index) {
-      const size = SIZES.find(s => s.slug === field.slug.value);
-      const inputID = `${id}-${size.slug}`;
-
-      return (
-        <li className="c--size-picker__size" key={index}>
-          <input className="c--size-picker__size__input" id={inputID} type="checkbox" {...field.isSelected} value={null} checked={field.isSelected.value} />
-          <label className="c--size-picker__size__label" htmlFor={inputID}>{size.name}</label>
-          <input type="hidden" {...field.slug} />
-        </li>
-      );
-    });
 
     const fieldErrors = fields.reduce(function(previous, field) {
       if (field.touched && field.error) {
@@ -46,9 +33,33 @@ const SizePicker = React.createClass({
       <div className={classes.join(' ')}>
         {errorTag}
 
-        <ul className="c--size-picker__sizes">
-          {sizeTags}
-        </ul>
+        <div className="c--size-picker__groups">
+          {SIZE_GROUPS.map(function(group, index) {
+            return (
+              <fieldset className="c--size-picker__group" key={index}>
+                <legend className="c--size-picker__group__name">{group.name}</legend>
+                <ul className="c--size-picker__group__sizes">
+                  {group.sizes.reduce(function(sizes, size, sizeIndex) {
+                    const field = fields.find(f => f.slug.value === size.slug);
+                    const inputID = `${id}-${size.slug}`;
+
+                    if (!field) { return sizes; }
+
+                    sizes.push(
+                      <li className="c--size-picker__size" key={sizeIndex}>
+                        <input className="c--size-picker__size__input" id={inputID} type="checkbox" {...field.isSelected} value={null} checked={field.isSelected.value} />
+                        <label className="c--size-picker__size__label" htmlFor={inputID}>{size.name}</label>
+                        <input type="hidden" {...field.slug} />
+                      </li>
+                    );
+
+                    return sizes;
+                  }, [])}
+                </ul>
+              </fieldset>
+            );
+          })}
+        </div>
       </div>
     );
   }
