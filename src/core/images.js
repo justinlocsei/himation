f979f434e03f2ef02d7e7b44f3003959a8104afe
Justin Locsei <justin.locsei@gmail.com>
2016-07-29 13:27:24 -0400

    Add a function for building an srcset attribute from image sizes

---
+++ src/core/images.js
@@ -0,0 +1,23 @@
+/**
+ * A size for a single image
+ *
+ * @typedef {object} HimationImageSize
+ * @property {string} path The path to the image
+ * @property {string} size The size descriptor for the image
+ */
+
+/**
+ * Convert a series of image sizes to a srcset value
+ *
+ * @param {HimationImageSize[]} imageSizes Size/path combinations for images
+ * @returns {string} A valid srcset attribute
+ */
+function imageSizesToSrcset(imageSizes) {
+  return imageSizes
+    .map(imageSize => imageSize.path + ' ' + imageSize.size)
+    .join(', ');
+}
+
+module.exports = {
+  imageSizesToSrcset: imageSizesToSrcset
+};
