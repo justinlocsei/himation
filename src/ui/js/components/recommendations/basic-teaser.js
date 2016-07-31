57e9d155e4bbef11ad47b35f46f89917866cf866
Justin Locsei <justin.locsei@gmail.com>
2016-07-30 21:48:31 -0400

    Scroll the page to a basic chosen from the table of contents

--- src/ui/js/components/recommendations/basic-teaser.js
+++ src/ui/js/components/recommendations/basic-teaser.js
@@ -1,3 +1,4 @@
+import anime from 'animejs';
 import React, { PropTypes } from 'react';
 
 const BasicTeaser = React.createClass({
@@ -10,13 +11,28 @@ const BasicTeaser = React.createClass({
     name: PropTypes.string.isRequired
   },
 
+  handleClick: function(e) {
+    e.preventDefault();
+
+    const basic = document.getElementById(this.props.anchorId);
+    const bounding = basic.getBoundingClientRect();
+    const basicTop = bounding.top + window.pageYOffset;
+
+    anime({
+      targets: ['body'],
+      scrollTop: basicTop,
+      duration: 1000,
+      easing: 'easeOutCubic'
+    });
+  },
+
   render: function() {
     const { anchorId, category, groupNumber, image, name } = this.props;
 
     const mediaStyle = {backgroundImage: `url(${image})`};
 
     return (
-      <a className="c--basic-teaser" href={`#${anchorId}`} data-group-number={groupNumber}>
+      <a className="c--basic-teaser" href={`#${anchorId}`} data-group-number={groupNumber} onClick={this.handleClick}>
         <div className="c--basic-teaser__media" style={mediaStyle}></div>
         <div className="c--basic-teaser__meta">
           <p className="c--basic-teaser__category">{category}</p>
