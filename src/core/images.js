08cd6a65c5dce2cb09d3ca10f27c09187f95727c
Justin Locsei <justin.locsei@gmail.com>
2016-08-09 19:52:46 -0400

    Add a function to get display dimensions for a set of images

--- src/core/images.js
+++ src/core/images.js
@@ -1,11 +1,23 @@
 'use strict';
 
+var sortBy = require('lodash/sortBy');
+
 /**
  * A size for a single image
  *
  * @typedef {object} HimationImageSize
+ * @property {number} height The height of the image
  * @property {string} path The path to the image
  * @property {string} size The size descriptor for the image
+ * @property {number} width The width of the image
+ */
+
+/**
+ * Dimensions for an image
+ *
+ * @typedef {object} HimationImageDimensions
+ * @property {number} height The height of the image
+ * @property {number} width The width of the image
  */
 
 /**
@@ -20,6 +32,24 @@ function imageSizesToSrcset(imageSizes) {
     .join(', ');
 }
 
+/**
+ * Convert a series of image sizes to dimensions for a given display width
+ *
+ * @param {HimationImageSize[]} imageSizes Size/path combinations for images
+ * @param {number} width The display width for the image
+ * @returns {HimationImageDimensions} The display dimensions for the image
+ */
+function imageSizesToDimensions(imageSizes, width) {
+  var normalSize = sortBy(imageSizes, size => size.width)[0];
+  var aspectRatio = normalSize.width / normalSize.height;
+
+  return {
+    height: width / aspectRatio,
+    width: width
+  };
+}
+
 module.exports = {
+  imageSizesToDimensions: imageSizesToDimensions,
   imageSizesToSrcset: imageSizesToSrcset
 };
