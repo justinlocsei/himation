3929f94252b63a1c6fdc155ecb3c2d7bd147c303
Justin Locsei <justin.locsei@gmail.com>
2016-08-08 20:12:34 -0400

    Update the package script to remove fluctuating shrinkwrap fields
    The `resolved` and `from` fields vary in a largely random manner, and 
    are not required to install dependencies.

--- scripts/package.js
+++ scripts/package.js
@@ -7,30 +7,83 @@ var fs = require('fs');
 var jsonStableStringify = require('json-stable-stringify');
 var path = require('path');
 
-var packageDir = path.normalize(path.join(__dirname, '..'));
-var shrinkwrapPath = path.join(packageDir, 'npm-shrinkwrap.json');
+var PACKAGE_DIR = path.normalize(path.join(__dirname, '..'));
+var SHRINKWRAP_PATH = path.join(PACKAGE_DIR, 'npm-shrinkwrap.json');
 
 console.log('Updating shrinkwrap...');
+var oldDeps = parseShrinkwrap();
+childProcess.execSync('npm shrinkwrap --dev', {cwd: PACKAGE_DIR});
+var newDeps = parseShrinkwrap();
+var dependencies = newDeps.dependencies;
 
-childProcess.execSync('npm shrinkwrap --dev', {
-  cwd: packageDir
-});
+console.log('Removing local links...');
+ignoreLinkedPackages(dependencies);
 
-console.log('Removing local dependencies...');
-
-var shrinkwrap = JSON.parse(fs.readFileSync(shrinkwrapPath));
-var dependencies = shrinkwrap.dependencies;
-
-Object.keys(dependencies).forEach(function(dependency) {
-  var details = dependencies[dependency];
-  if (!details.resolved) {
-    delete dependencies[dependency];
-  }
-});
+console.log('Removing extraneous fields...');
+removeExtraneousFields(dependencies, oldDeps.dependencies);
 
 console.log('Writing updated shrinkwrap...');
-
-var json = jsonStableStringify(shrinkwrap, {space: 2});
-fs.writeFileSync(shrinkwrapPath, json);
+var json = jsonStableStringify(newDeps, {space: 2});
+fs.writeFileSync(SHRINKWRAP_PATH, json);
 
 console.log('Package updated!');
+
+/**
+ * Get an in-memory representation of the shrinkwrap file
+ *
+ * @returns {object} The shrinkwrap data
+ */
+function parseShrinkwrap() {
+  if (fs.statSync(SHRINKWRAP_PATH).isFile()) {
+    return JSON.parse(fs.readFileSync(SHRINKWRAP_PATH));
+  } else {
+    return {
+      dependencies: {}
+    };
+  }
+}
+
+/**
+ * Remove all links from the top-level dependencies
+ *
+ * Links show up in the manifest as lacking a resolution, so we use an empty
+ * `resolved` field as the hint that the package is a link.
+ *
+ * @param {object} dependencies The package dependencies
+ */
+function ignoreLinkedPackages(dependencies) {
+  Object.keys(dependencies).forEach(function(dependency) {
+    var details = dependencies[dependency];
+    if (!details.resolved) {
+      delete dependencies[dependency];
+    }
+  });
+}
+
+/**
+ * Remove all extraneous fields from each dependency in the tree
+ *
+ * This removes the `resolved` and `from` fields for all non-git dependencies,
+ * and ensures that git dependencies remain unmodified.
+ *
+ * @param {object} newDeps The current package dependencies
+ * @param {object} oldDeps The previous package dependencies
+ */
+function removeExtraneousFields(newDeps, oldDeps) {
+  Object.keys(newDeps).forEach(function(depName) {
+    var newDep = newDeps[depName];
+    var oldDep = oldDeps[depName];
+
+    if (newDep.resolved && newDep.resolved.indexOf('git:') === -1) {
+      delete newDep.resolved;
+      delete newDep.from;
+    } else if (oldDep) {
+      newDep.resolved = oldDep.resolved;
+      newDep.from = oldDep.from;
+    }
+
+    if (newDep.dependencies) {
+      removeExtraneousFields(newDep.dependencies, oldDep ? oldDep.dependencies || {} : {});
+    }
+  });
+}
