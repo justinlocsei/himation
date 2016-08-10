14453d0651360f680fcf391add77ddabde24ad26
Justin Locsei <justin.locsei@gmail.com>
2016-08-10 18:46:55 -0400

    Add a React extension to check if props have changed

--- src/core/extensions/react.js
+++ src/core/extensions/react.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var isEqual = require('lodash/isEqual');
 var PropTypes = require('react').PropTypes;
 
 var childProps = {
@@ -10,6 +11,21 @@ var childProps = {
   single: PropTypes.element
 };
 
+/**
+ * Determine if any props have changed between two objects
+ *
+ * @param {object} current The current object
+ * @param {object} next The next object
+ * @param {string[]} values The values to check
+ * @returns {boolean} Whether any values have changed
+ */
+function anyPropsChanged(current, next, values) {
+  return values.reduce(function(isChanged, value) {
+    return isChanged || !isEqual(current[value], next[value]);
+  }, false);
+}
+
 module.exports = {
+  anyPropsChanged: anyPropsChanged,
   childProps: childProps
 };
