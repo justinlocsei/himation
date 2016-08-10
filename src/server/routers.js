92c865cd71ec78407c9a53a99b8a888b9ccb61de
Justin Locsei <justin.locsei@gmail.com>
2016-08-10 09:17:16 -0400

    Pre-load compiled route handlers

--- src/server/routers.js
+++ src/server/routers.js
@@ -3,16 +3,15 @@
 var express = require('express');
 
 /**
- * Mock a browser environment on the server
+ * Load a compiled route handler from the build manifest
  *
- * Since server-side router handlers require UI code that may contain libraries
- * that make assumptions about being in a browser environment, we need to mock
- * out the `window` object on the server.
- *
+ * @param {HimationBuildManifest} build The build manifest for the server's files
+ * @param {HimationRoute} route A single route
+ * @returns {object} The route handler
  * @private
  */
-function mockBrowserEnvironment() {
-  global.window = global.window || {};
+function loadRouteHandler(build, route) {
+  return require(build.entries[route.guid]);
 }
 
 /**
@@ -23,6 +22,12 @@ function mockBrowserEnvironment() {
  * defined in the build manifest, and these entry files are lazily loaded when
  * the route is requested.
  *
+ * Since server-side router handlers require UI code that may contain libraries
+ * that make assumptions about being in a browser environment, we need to mock
+ * out the `window` object on the server.  We then require the compiled route JS
+ * files from the build manifest, which allows the subsequent `require` calls
+ * in each Express handler to avoid a costly initial load.
+ *
  * @param {HimationBuildManifest} build The build manifest for the server's files
  * @param {HimationRoute[]} routes All available routes
  * @param {HimationSettings} settings The current settings
@@ -31,11 +36,15 @@ function mockBrowserEnvironment() {
 function create(build, routes, settings) {
   var router = express.Router(); // eslint-disable-line new-cap
 
+  global.window = global.window || {};
+
+  routes.forEach(function(route) {
+    loadRouteHandler(build, route);
+  });
+
   routes.forEach(function(route) {
     router[route.method](route.path, function(req, res, next) {
-      mockBrowserEnvironment();
-
-      var handler = require(build.entries[route.guid]);
+      var handler = loadRouteHandler(build, route);
       handler.renderResponse(req, res, next, settings);
     });
   });
