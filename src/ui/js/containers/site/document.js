60564fb0afe7e7fa8fe8bc10a7647fd6b6a2a80b
Justin Locsei <justin.locsei@gmail.com>
2016-08-10 20:15:12 -0400

    Inject Google Analytics tracking code into all pages

--- src/ui/js/containers/site/document.js
+++ src/ui/js/containers/site/document.js
@@ -2,6 +2,9 @@ import React, { PropTypes } from 'react';
 
 import favicon from 'himation/images/branding/favicon.ico';
 
+// This global variable is replaced at build time with the Google Analytics ID
+const GOOGLE_ANALYTICS_ID = HIMATION_GOOGLE_ANALYTICS_ID;
+
 const Document = React.createClass({
 
   propTypes: {
@@ -65,6 +68,8 @@ const Document = React.createClass({
           <script dangerouslySetInnerHTML={this._exposeReduxState()} />
 
           {jsTags}
+
+          <script dangerouslySetInnerHTML={this._addGoogleAnalyticsTracking()} />
         </body>
 
       </html>
@@ -81,6 +86,25 @@ const Document = React.createClass({
 
     const markup = `window['${stateVariableName}'] = ${JSON.stringify(store)};`;
     return {__html: markup};
+  },
+
+  /**
+   * Product JavaScript that loads Google Analytics
+   *
+   * @returns {object} Data that be used to dangerously set the HTML of a script tag
+   */
+  _addGoogleAnalyticsTracking: function() {
+    const script = `
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', '${GOOGLE_ANALYTICS_ID}', 'auto');
+      ga('send', 'pageview');
+    `;
+
+    return {__html: script};
   }
 
 });
