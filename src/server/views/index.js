e9c8672a7f2377cf0dfbee0a1e312b309cf78e25
Justin Locsei <justin.locsei@gmail.com>
2016-08-11 10:49:08 -0400

    Mark the index page as cacheable

--- src/server/views/index.js
+++ src/server/views/index.js
@@ -1,8 +1,9 @@
 import IndexPage from 'himation/ui/containers/pages';
+import { allowGatewayCaching } from 'himation/server/caching';
 import { prerenderPageComponent } from 'himation/ui/rendering';
 import { renderHtml } from 'himation/server/rendering';
 
-export function renderResponse(req, res) {
+export function renderResponse(req, res, next, settings) {
   const markup = prerenderPageComponent(res, IndexPage);
-  res.send(renderHtml(markup));
+  allowGatewayCaching(res, settings.cacheTtl).send(renderHtml(markup));
 }
