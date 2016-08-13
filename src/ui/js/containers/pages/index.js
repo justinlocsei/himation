7b651b2bf3f21d31a6382eb615b1d80ac52bf0c3
Justin Locsei <justin.locsei@gmail.com>
2016-08-13 19:06:48 -0400

    Add a pitch to the home page

--- src/ui/js/containers/pages/index.js
+++ src/ui/js/containers/pages/index.js
@@ -7,6 +7,8 @@ import { guidToRoute } from 'himation/core/routing';
 
 import 'himation/styles/survey';
 
+const SURVEY_ID = 'survey';
+
 const HomePage = React.createClass({
 
   render: function() {
@@ -14,7 +16,26 @@ const HomePage = React.createClass({
 
     return (
       <Page>
-        <Survey formAction={submitSurvey.path} formMethod={submitSurvey.method} />
+
+        <div className="l--pitch">
+          <h1 className="l--pitch__tagline">
+            Ladies, take the pain out of keeping your professional wardrobe
+            stocked with essentials.
+          </h1>
+          <p className="l--pitch__details">
+            We use your shape, style, and workplace formality to provide you
+            with the best clothes available from a wide range of retailers.
+            There’s no charge to use our site and no markup to the clothing. So
+            get started now and never spend your weekend shopping for work
+            clothes again.
+          </p>
+          <div className="l--pitch__cta">
+            <a href={`#${SURVEY_ID}`} className="l--pitch__button">Fill Out Our Survey</a>
+          </div>
+        </div>
+
+        <Survey formAction={submitSurvey.path} formMethod={submitSurvey.method} anchorId={SURVEY_ID} />
+
       </Page>
     );
   }
