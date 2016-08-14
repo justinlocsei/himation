c4b25cdab392f26847d49ee8df4825c6eb331a36
Justin Locsei <justin.locsei@gmail.com>
2016-08-14 04:23:22 -0400

    Use an image with the pitch

--- src/ui/js/components/pitch.js
+++ src/ui/js/components/pitch.js
@@ -1,5 +1,7 @@
 import React, { PropTypes } from 'react';
 
+const businesswoman = require('himation/images/pitch/businesswoman-2x.jpg');
+
 const Pitch = React.createClass({
 
   propTypes: {
@@ -15,26 +17,34 @@ const Pitch = React.createClass({
   render: function() {
     const { surveyId } = this.props;
 
+    const graphicStyles = {
+      backgroundImage: `url(${businesswoman.src})`
+    };
+
     return (
       <div className="l--pitch">
+        <div className="l--pitch__content">
 
-        <h1 className="l--pitch__tagline">
-          Ladies, take the pain out of keeping your professional wardrobe
-          stocked with essentials.
-        </h1>
+          <h1 className="l--pitch__tagline">
+            Ladies, take the pain out of keeping your professional wardrobe
+            stocked with essentials.
+          </h1>
 
-        <p className="l--pitch__details">
-          We use your shape, style, and workplace formality to provide you
-          with the best clothes available from a wide range of retailers.
-          There’s no charge to use our site and no markup to the clothing. So
-          get started now and never spend your weekend shopping for work
-          clothes again.
-        </p>
+          <p className="l--pitch__details">
+            We use your shape, style, and workplace formality to provide you
+            with the best clothes available from a wide range of retailers.
+            There’s no charge to use our site and no markup to the clothing. So
+            get started now and never spend your weekend shopping for work
+            clothes again.
+          </p>
 
-        <div className="l--pitch__cta">
-          <a href={`#${surveyId}`} className="l--pitch__button" onClick={this.handleClick}>Fill Out Our Survey</a>
+          <div className="l--pitch__cta">
+            <a href={`#${surveyId}`} className="l--pitch__button" onClick={this.handleClick}>Fill Out Our Survey</a>
+          </div>
+
         </div>
 
+        <div className="l--pitch__graphic" style={graphicStyles} />
       </div>
     );
   }
