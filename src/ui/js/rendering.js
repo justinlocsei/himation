412d7b691f4884ec6c462c3ab2b0fa2e3937cfdf
Justin Locsei <justin.locsei@gmail.com>
2016-08-28 20:28:22 -0400

    Add a meta tag for the Facebook app ID if it is present

--- src/ui/js/rendering.js
+++ src/ui/js/rendering.js
@@ -97,6 +97,7 @@ export function prerenderPageComponent(res, Page, options = {}) {
     copyrightYear: new Date().getFullYear(),
     facebookShareImage: facebookShareImage.src,
     favicon: favicon,
+    facebookAppId: getSetting('facebookAppId'),
     googleAnalyticsId: getSetting('googleAnalyticsId'),
     logo: logo,
     modernizrUrl: modernizrUrl,
