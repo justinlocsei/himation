fa630de92a644e368ec31a51d8bb5e96a6d2dbfb
Justin Locsei <justin.locsei@gmail.com>
2016-09-05 21:10:51 -0400

    Remove test paths

--- src/core/paths.js
+++ src/core/paths.js
@@ -9,7 +9,6 @@ var root = path.normalize(path.join(__dirname, '..', '..'));
 var build = path.join(root, 'build');
 var modules = path.join(root, 'node_modules');
 var src = path.join(root, 'src');
-var test = path.join(root, 'test');
 
 var server = path.join(src, 'server');
 var ui = path.join(src, 'ui');
@@ -43,10 +42,6 @@ function resolve() {
     },
     settings: settingsPath,
     src: src,
-    test: {
-      functional: path.join(test, 'functional'),
-      root: test
-    },
     ui: {
       images: path.join(ui, 'images'),
       js: path.join(ui, 'js'),
