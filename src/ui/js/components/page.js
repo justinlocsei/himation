db987ff925a2c52dab054ae912784f2fa5aa1213
Justin Locsei <justin.locsei@gmail.com>
2016-09-10 15:01:10 -0400

    Allow pages to have custom slugs

--- src/ui/js/components/page.js
+++ src/ui/js/components/page.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 import { childProps } from 'himation/core/extensions/react';
 
 const Page = React.createClass({
 
   propTypes: {
-    children: childProps.multiple
+    children: childProps.multiple,
+    slug: PropTypes.string
   },
 
   render: function() {
-    const { children } = this.props;
+    const { children, slug } = this.props;
+
+    const classes = ['l--page'];
+    if (slug) { classes.push(`for-${slug}`); }
 
     return (
-      <div className="l--page">
+      <div className={classes.join(' ')}>
         <main className="l--page__content">
           {children}
         </main>
