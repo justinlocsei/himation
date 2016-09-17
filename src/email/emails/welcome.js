f5ad572bec1dd2d064a7cf45b791af18d1ad2991
Justin Locsei <justin.locsei@gmail.com>
2016-09-16 20:55:22 -0400

    Define tags and a campaign name for the welcome email

--- src/email/emails/welcome.js
+++ src/email/emails/welcome.js
@@ -4,7 +4,12 @@ var emails = require('himation/email/emails');
 
 var WelcomeEmail = emails.defineEmail({
 
+  campaignName: 'Welcome',
   name: 'Welcome',
+
+  getRecipientTags: function() {
+    return ['welcome'];
+  },
 
   getSubject: function() {
     return 'Welcome to Cover Your Basics';
