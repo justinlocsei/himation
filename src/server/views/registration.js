d51fa030b915ce4aec3ac386a963ce3b19c503cf
Justin Locsei <justin.locsei@gmail.com>
2016-10-13 11:10:23 -0400

    Set registered cookies to last for a year

--- src/server/views/registration.js
+++ src/server/views/registration.js
@@ -29,7 +29,7 @@ module.exports = function renderResponse(req, res) {
 
       res.cookie(settings.cookies.registered, '1', {
         httpOnly: true,
-        maxAge: 90000,
+        maxAge: 60 * 60 * 24 * 365,
         secure: true
       });
 
