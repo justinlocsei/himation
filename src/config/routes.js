fa5c0a93835c52431d2cb82ef7dbb06a8f1e462b
Justin Locsei <justin.locsei@gmail.com>
2017-01-04 20:31:25 -0500

    Add a route for the survey anchor

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,11 +1,13 @@
 'use strict';
 
+var constants = require('himation/core/constants');
 var routing = require('himation/core/routing');
 
 module.exports = routing.defineRoutes([{
   name: 'himation',
   path: '/',
   paths: [
+    {path: '#' + constants.SURVEY_ANCHOR, name: 'survey', method: 'get'},
     {path: 'about', name: 'about', method: 'get'},
     {path: 'recommendations', name: 'recommendations', method: 'post'},
     {path: 'register', name: 'registration', method: 'post'}
