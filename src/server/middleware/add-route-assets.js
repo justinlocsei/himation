3930efcc82bded4106bbf5f18873655be2b88ae6
Justin Locsei <justin.locsei@gmail.com>
2017-01-22 14:28:59 -0500

    Send preloading link headers with all requests

--- src/server/middleware/add-route-assets.js
+++ src/server/middleware/add-route-assets.js
@@ -11,6 +11,12 @@ var ASSET_GROUPS = {
   stylesheets: ['.css']
 };
 
+// A map between asset groups and preload destinations
+var ASSET_PRELOAD_DESTINATIONS = {
+  javascripts: 'script',
+  stylesheets: 'style'
+};
+
 /**
  * Create a middleware function that adds assets to the template context
  *
@@ -28,7 +34,7 @@ function create(manifest, host, routes) {
     var route = routing.pathToRoute(routes, req.path, req.method);
     if (!route) { return next(); }
 
-    res.locals.assets = Object.keys(ASSET_GROUPS).reduce(function(groups, group) {
+    var assetTypes = Object.keys(ASSET_GROUPS).reduce(function(groups, group) {
       var matches = ASSET_GROUPS[group].map(extension => escapeRegExp(extension) + '$');
       var extensionMatcher = new RegExp(matches.join('|'));
 
@@ -38,6 +44,15 @@ function create(manifest, host, routes) {
 
       return groups;
     }, {});
+
+    var linkHeaders = Object.keys(assetTypes).reduce(function(headers, type) {
+      return headers.concat(assetTypes[type].map(function(assetUrl) {
+        return '<' + assetUrl + '>; rel=preload; as=' + ASSET_PRELOAD_DESTINATIONS[type];
+      }));
+    }, []);
+
+    res.locals.assets = assetTypes;
+    res.set('Link', linkHeaders);
 
     return next();
   };
